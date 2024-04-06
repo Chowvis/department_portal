f2388d5f29d778fdbb117b46764483f58b581906
Kenmar Dirchi <64697482+KNMR-GITHUB@users.noreply.github.com>
2024-04-06 18:00:23 +0530

    sdfsd

--- resources/views/homepage/section5.blade.php
+++ resources/views/homepage/section5.blade.php
@@ -5,6 +5,21 @@
     </div>
     <div class="w-1/2 flex flex-col">
         <div id="notice" class="border border-blue-500">Notice</div>
-        <div id="notice_contents" class="h-56 border border-blue-500"></div>
+        <div id="notice_contents" class="h-56 border border-blue-500">
+            <ol>
+                <li>a</li>
+                <li>b</li>
+                <li>c</li>
+                <li>d</li>
+                <li>e</li>
+                <li>f</li>
+                <li>g</li>
+
+            </ol>
+            <div id="read_more" class="mt-2">
+                <p class="">View All</p>
+            </div>
+        </div>
+
     </div>
 </div>
