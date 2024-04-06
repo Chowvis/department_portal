9bc167de8cc0f09c48cf947407e141326711e7c5
Kenmar Dirchi <64697482+KNMR-GITHUB@users.noreply.github.com>
2024-04-06 21:22:47 +0530

    responsive, added images for gallery, simple layout adjustments

--- resources/views/homepage/section5.blade.php
+++ resources/views/homepage/section5.blade.php
@@ -1,23 +1,23 @@
-<div class="font-bold max-w-[1280px] w-full flex border-2 border-green-500">
-    <div class="w-1/2 flex flex-col">
+<div class=" max-w-[1280px] w-full flex flex-col md:flex-row border-2 border-green-500">
+    <div class="w-full md:w-1/2 flex flex-col">
         <div id="blog" class="border border-blue-500">Blog</div>
         <div id="content_for_blog" class="h-56 border border-blue-500"></div>
     </div>
-    <div class="w-1/2 flex flex-col">
-        <div id="notice" class="border border-blue-500">Notice</div>
-        <div id="notice_contents" class="h-56 border border-blue-500">
-            <ol>
-                <li>a</li>
-                <li>b</li>
-                <li>c</li>
-                <li>d</li>
-                <li>e</li>
-                <li>f</li>
-                <li>g</li>
+    <div class="w-full md:w-1/2 flex flex-col">
+        <div id="notice" class="border border-blue-500 px-3 font-semibold">Notice</div>
+        <div id="notice_contents" class="h-56 border border-blue-500 p-3">
+            <ul class="list-disc list-inside">
+                <li>Title of new scheme, service</li>
+                <li>Land Records</li>
+                <li>Employment Exchange</li>
+                <li>Disability Commision</li>
+                <li>How to lodge a Grievance</li>
+                <li>Apply for Death Certificate</li>
+                <li>Residence Certificate</li>
 
-            </ol>
-            <div id="read_more" class="mt-2">
-                <p class="">View All</p>
+            </ul>
+            <div id="read_more" class=" bg-blue-400 text-black hover:text-white p-2 w-fit">
+                View All
             </div>
         </div>
 
--- resources/views/homepage/section7.blade.php
+++ resources/views/homepage/section7.blade.php
@@ -1,10 +1,13 @@
-<div class="font-bold max-w-[1280px] w-full flex">
-    <div class="w-1/2 flex flex-col">
-        <div id="blog" class="text-lg font-semibold border border-blue-500">Photo Gallery</div>
-        <div id="content_for_blog" class=" h-56 border border-blue-500"></div>
+<div class=" max-w-[1280px] w-full flex flex-col md:flex-row">
+    <div class="w-full md:w-1/2 flex flex-col">
+        <div id="photo_gallery" class="text-lg  border border-blue-500 flex justify-between">
+            <p class="font-semibold">Photo Gallery</p>
+            <p class="bg-orange-400 text-white hover:bg-orange-600 px-2">View All</p>
+        </div>
+        <div id="content_for_gallery" class=" h-56 border border-blue-500"></div>
     </div>
-    <div class="w-1/2 flex flex-col">
-        <div id="notice" class="text-lg font-semibold border border-blue-500">Calender</div>
-        <div id="notice_contents" class="h-56 border border-blue-500"></div>
+    <div class="w-full md:w-1/2 flex flex-col">
+        <div id="calender" class="text-lg font-semibold border border-blue-500">Calender</div>
+        <div id="content_for_calender" class=" h-56 border border-blue-500"></div>
     </div>
 </div>
